f854325e620a663879c888103999fdd4e8f552dc
Lucian Torje <lucian.torje@siemens.com>
2022-07-15 06:01:59 +0300

    fix checkstyle

--- core/policy/policy-evaluator/src/test/java/org/eclipse/dataspaceconnector/policy/model/PolicyDefinitionTest.java
+++ core/policy/policy-evaluator/src/test/java/org/eclipse/dataspaceconnector/policy/model/PolicyDefinitionTest.java
@@ -1,3 +1,17 @@
+/*
+ *  Copyright (c) 2021 Microsoft Corporation
+ *
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Apache License, Version 2.0 which is available at
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  Contributors:
+ *       Microsoft Corporation - initial API and implementation
+ *
+ */
+
 package org.eclipse.dataspaceconnector.policy.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
@@ -10,7 +24,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 class PolicyDefinitionTest {
     @Test
     void deserialize() throws JsonProcessingException {
-        var policyJson = "{\"uid\":\"uid1\",\"policy\":{\"permissions\":[{\"edctype\":\"dataspaceconnector:permission\",\"uid\":\"permissionUid\",\"constraints\":[],\"duties\":[]}],\"prohibitions\":[],\"obligations\":[],\"extensibleProperties\":{},\"target\":\"assetId\",\"@type\":{\"@policytype\":\"contract\"}}}";
+        var policyJson = "{\"uid\":\"uid1\",\"policy\":{\"permissions\":[{\"edctype\":\"dataspaceconnector:permission\",\"uid\":\"permissionUid\"," +
+                "\"constraints\":[],\"duties\":[]}],\"prohibitions\":[],\"obligations\":[],\"extensibleProperties\":{}," +
+                "\"target\":\"assetId\",\"@type\":{\"@policytype\":\"contract\"}}}";
         var mapper = new ObjectMapper();
 
         var policyDefinition =  mapper.readValue(policyJson, PolicyDefinition.class);
@@ -38,6 +54,8 @@ class PolicyDefinitionTest {
 
         var policyJson =  mapper.writeValueAsString(policyDefinition);
 
-        assertThat(policyJson).hasToString("{\"uid\":\"uid1\",\"policy\":{\"permissions\":[{\"edctype\":\"dataspaceconnector:permission\",\"uid\":\"permissionUid\",\"constraints\":[],\"duties\":[]}],\"prohibitions\":[],\"obligations\":[],\"extensibleProperties\":{},\"target\":\"assetId\",\"@type\":{\"@policytype\":\"contract\"}}}");
+        assertThat(policyJson).hasToString("{\"uid\":\"uid1\",\"policy\":{\"permissions\":[{\"edctype\":\"dataspaceconnector:permission\"," +
+                "\"uid\":\"permissionUid\",\"constraints\":[],\"duties\":[]}],\"prohibitions\":[],\"obligations\":[]," +
+                "\"extensibleProperties\":{},\"target\":\"assetId\",\"@type\":{\"@policytype\":\"contract\"}}}");
     }
 }
